45a321bf46bab344331370fb9e1d30afea044da1
Liam Carswell <liamcarswell@gmail.com>
2022-06-17 12:04:21 +0100

    add filter options to topics pages

--- src/components/Topic/Topic.jsx
+++ src/components/Topic/Topic.jsx
@@ -1,14 +1,17 @@
 import Filter from "../Home/Filter"
 import ArticleGrid from "../Home/ArticleGrid/ArticleGrid"
 import { useParams } from "react-router-dom"
+import { useState } from "react"
 
 const Topic = () => {
     const { topic } = useParams()
+    const [sortBy, setSortBy] = useState(null)
+
     return (
         <main>
             <h2>All {topic} articles</h2>
-            <Filter />
-            <ArticleGrid topic={topic}/>
+            <Filter sortBy={sortBy} setSortBy={setSortBy}/>
+            <ArticleGrid topic={topic} sortBy={sortBy}/>
         </main>
     )
 }
